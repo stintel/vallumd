stintel/vallumd

commit 69824bec3747aec06d21279bfdeda4dae066712e

ipset: support libipset version 7
Add support for libipset version 7, while maintaining backward
compatibility with version 6. Try to detect the version with pkg-config,
but also add an option to manually enable version 6 compatibility for
distros where libipset pkg-config is broken.

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/ipset.c
@@ -21,14 +21,23 @@
 #include <arpa/inet.h>
 #include <libipset/session.h>
 #include <libipset/types.h>
-#include <libipset/ui.h>
 #include <string.h>
+
+#ifdef WITH_LIBIPSET_V6_COMPAT
+#include <libipset/ui.h>
+#else
+#include <libipset/ipset.h>
+#endif
 
 #include "log.h"
 
 static int exit_error(int e, struct ipset_session *sess)
 {
+#ifdef WITH_LIBIPSET_V6_COMPAT
     pr_err("ipset: %s\n", ipset_session_error(sess));
+#else
+    ipset_session_report(sess, ipset_session_report_type(sess), "ipset: %s\n");
+#endif
     ipset_session_fini(sess);
 
     return e;
@@ -60,17 +69,25 @@ int ipset_do(int c, char *set, char *elem)
 
     ipset_load_types();
 
+#ifdef WITH_LIBIPSET_V6_COMPAT
     sess = ipset_session_init(printf);
+#else
+    sess = ipset_session_init(NULL, NULL);
+#endif
     if (sess == NULL) {
         pr_err("ipset: failed to initialize session\n");
         return 1;
     }
 
     if (cmd == IPSET_CMD_ADD) {
+#ifdef WITH_LIBIPSET_V6_COMPAT
         ret = ipset_envopt_parse(sess, IPSET_ENV_EXIST, NULL);
         if (ret < 0) {
             return exit_error(1, sess);
         }
+#else
+        ipset_envopt_set(sess, IPSET_ENV_EXIST);
+#endif
     }
 
     ret = ipset_parse_setname(sess, IPSET_SETNAME, set);

CMakeLists.txt
@@ -41,6 +41,13 @@ include_directories("${PROJECT_BINARY_DIR}")
 
 find_package(PkgConfig REQUIRED)
 
+pkg_check_modules(LIBIPSET QUIET libipset)
+
+option (WITH_LIBIPSET_V6_COMPAT "Enable compatibility with libipset version 6" OFF)
+if ((LIBIPSET_FOUND AND LIBIPSET_VERSION VERSION_LESS 7) OR WITH_LIBIPSET_V6_COMPAT)
+    add_definitions("-DWITH_LIBIPSET_V6_COMPAT")
+endif ()
+
 option (WITH_TLS "Enable TLS support" ON)
 if (${WITH_TLS} STREQUAL ON)
     pkg_search_module(LIBSSL QUIET "libssl")

commit a83aaaf26b471df891fec04dab50aed24dbaf3c8

ipset: fix segfault in exit_error()
Calling ipset_session_report with a format specifier but without an
argument throws SIGSEGV in vsnprintf on some distributions.

Fix this by using pr_err with the error returned by
ipset_session_report_msg similar to what we do for libipset v6.

Fixes: #16
Fixes: 69824bec3747 ("ipset: support libipset version 7")
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/ipset.c
@@ -22,7 +22,7 @@ static int exit_error(int e, struct ipset_session *sess)
 #ifdef WITH_LIBIPSET_V6_COMPAT
     pr_err("ipset: %s\n", ipset_session_error(sess));
 #else
-    ipset_session_report(sess, ipset_session_report_type(sess), "ipset: %s\n");
+    pr_err("ipset: %s\n", ipset_session_report_msg(sess));
 #endif
     ipset_session_fini(sess);
 