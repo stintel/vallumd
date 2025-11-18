ec7a57929332899cbe9fff4418751864a3597a4b
Stijn Tintel <stijn@linux-ipv6.be>
2025-11-18 14:00:52 +0200

    inet: drop extern from function definitions
    Function definitions have external linkage by default, so this is
    redundant.
    
    Suggested-by: Wouter Paesen <wouter@blue-gate.be>
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- src/inet.c
+++ src/inet.c
@@ -6,7 +6,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-extern int get_inet_family(char *ipaddr)
+int get_inet_family(char *ipaddr)
 {
     unsigned char dst[sizeof(struct in6_addr)];
     unsigned int family = 0;
@@ -23,7 +23,7 @@ extern int get_inet_family(char *ipaddr)
     }
 }
 
-extern int ip_valid(char *ipaddr)
+int ip_valid(char *ipaddr)
 {
     return get_inet_family(ipaddr) != AF_UNSPEC;
 }
