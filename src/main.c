stintel/vallumd

commit e55bade747dd073a6b7d6b297b614c42cca08e11

main: exit early when no topics are specified
Fixes the following clang-tidy warnings:

/home/stijn/projects/vallumd/src/main.c:56:23: warning: Call to 'malloc' has an allocation size of 0 bytes [clang-analyzer-optin.portability.UnixAPI]
   56 |     mqttconn.topics = malloc(mqttconn.ntopics * sizeof(*mqttconn.topics));
      |                       ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
/home/stijn/projects/vallumd/src/main.c:50:12: note: Assuming the condition is false
   50 |     while ((opt = getopt(argc, argv, "c:h:p:P:t:Tu:V")) != -1) {
      |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
/home/stijn/projects/vallumd/src/main.c:50:5: note: Loop condition is false. Execution continues on line 56
   50 |     while ((opt = getopt(argc, argv, "c:h:p:P:t:Tu:V")) != -1) {
      |     ^
/home/stijn/projects/vallumd/src/main.c:56:23: note: Call to 'malloc' has an allocation size of 0 bytes
   56 |     mqttconn.topics = malloc(mqttconn.ntopics * sizeof(*mqttconn.topics));
      |                       ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/main.c
@@ -54,6 +54,11 @@ int main(int argc, char **argv)
         }
     }
 
+    if (mqttconn.ntopics == 0) {
+        print_usage();
+        return 1;
+    }
+
     mqttconn.topics = malloc(mqttconn.ntopics * sizeof(*mqttconn.topics));
 
     optind = 0;
@@ -93,7 +98,7 @@ int main(int argc, char **argv)
         }
     }
 
-    if (mqttconn.host == NULL || mqttconn.ntopics == 0) {
+    if (mqttconn.host == NULL) {
         print_usage();
         return 1;
     }

commit 3709e420daeebc89ab59ea2b3e70495aae51cc53

main: fix version argument
Fixes: e55bade747dd ("main: exit early when no topics are specified")
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/main.c
@@ -49,7 +49,10 @@ int main(int argc, char **argv)
     openlog("vallumd", LOG_PID, LOG_DAEMON);
 
     while ((opt = getopt(argc, argv, "c:h:p:P:t:Tu:V")) != -1) {
-        if (opt == 't') {
+        if (opt == 'V') {
+            fprintf(stdout, "vallumd-%s\n", VERSION);
+            return 0;
+        } else if (opt == 't') {
             mqttconn.ntopics++;
         }
     }
@@ -88,9 +91,6 @@ int main(int argc, char **argv)
             case 'u':
                 mqttconn.username = optarg;
                 break;
-            case 'V':
-                fprintf(stdout, "vallumd-%s\n", VERSION);
-                return 0;
             default:
                 print_usage();
                 return 1;