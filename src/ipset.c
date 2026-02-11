stintel/vallumd

commit d7a0a6ac8eba1294baec1daaaf4768934d54f53d

ipset: compare address family of set and element
This produces a slightly clearer error message.

Before:
ipset: Syntax error: cannot parse fe80::1: resolving to IPv4 address failed

After:
ipset: address fe80::1 does not match family of set foo

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/ipset.c
@@ -33,7 +33,7 @@ int ipset_do(int c, char *set, char *elem)
 {
     const struct ipset_type *type = NULL;
     enum ipset_cmd cmd = c;
-    int ret = 0;
+    int family = 0, ret = 0;
     struct ipset_session *sess;
 
     if (!ip_valid(elem)) {
@@ -72,6 +72,12 @@ int ipset_do(int c, char *set, char *elem)
     type = ipset_type_get(sess, cmd);
     if (type == NULL) {
         return exit_error(1, sess);
+    }
+
+    family = ipset_data_family(ipset_session_data(sess));
+    if (family != get_inet_family(elem)) {
+        pr_err("ipset: address %s does not match family of set %s", elem, set);
+        return 1;
     }
 
     ret = ipset_parse_elem(sess, type->last_elem_optional, elem);

commit dd22df5ec0ea72e2736d31de198e1733608fb0f3

ipset: fix session leak on mismatched family
When address family doesn't match, we exit and skip ipset_session_fini,
leaking the ipset session. Fix this with a cleanup label and goto.

Fixes: d7a0a6ac8eba ("ipset: compare address family of set and element")
Reported-by: Claude Opus 4.6 <noreply@anthropic.com>
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/ipset.c
@@ -78,7 +78,8 @@ static int ipset_do(int command, const char *set, const char *elem)
     family = ipset_data_family(ipset_session_data(sess));
     if (family != get_inet_family(elem)) {
         pr_err("ipset: address %s does not match family of set %s", elem, set);
-        return 1;
+        ret = 1;
+        goto cleanup;
     }
 
     ret = ipset_parse_elem(sess, type->last_elem_optional, elem);
@@ -91,9 +92,10 @@ static int ipset_do(int command, const char *set, const char *elem)
         return exit_error(1, sess);
     }
 
+cleanup:
     ipset_session_fini(sess);
 
-    return 0;
+    return ret;
 }
 
 int ipset_add(const char *set, const char *elem)