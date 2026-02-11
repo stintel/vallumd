stintel/vallumd

commit efc23fe14e7716dda0b246c3df047849f428f53a

mosquitto: add message callback
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/mosquitto.c
@@ -21,6 +21,8 @@
 #include <mosquitto.h>
 #include <stdio.h>
 
+#include "ipset.h"
+
 #define mqtt_cid "vallumd"
 
 char *mqtt_topic;
@@ -29,6 +31,13 @@ void cb_con(struct mosquitto *m, void *userdata, int result)
 {
     if(!result) {
         mosquitto_subscribe(m, NULL, mqtt_topic, 2);
+    }
+}
+
+void cb_msg(struct mosquitto *m, void *userdata, const struct mosquitto_message *msg)
+{
+    if(msg->payloadlen) {
+        ipset_add("blacklist", msg->payload, 0);
     }
 }
 
@@ -42,6 +51,7 @@ int init_mqtt(char *mqtt_host, int mqtt_port) {
     m = mosquitto_new(mqtt_cid, clean_session, NULL);
 
     mosquitto_connect_callback_set(m, cb_con);
+    mosquitto_message_callback_set(m, cb_msg);
 
     if(mosquitto_connect(m, mqtt_host, mqtt_port, keepalive)) {
         fprintf(stderr, "Unable to connect to %s:%d\n", mqtt_host, mqtt_port);

commit c132d3510e6c64ed17dccdd3993470aadc07d76c

mosquitto: NUL-terminate payload
The payload in the Mosquitto callback is not guaranteed to be
NUL-terminated. Use strndup with the payload length to make sure we have
a proper NUL-terminated string before passing it to ipset.

Without this, a payload with binary data could cause an out-of-bounds
read. Confirmed by AddressSanitizer:

AddressSanitizer:DEADLYSIGNAL
Segmentation fault

Fixes: efc23fe14e77 ("mosquitto: add message callback")
Reported-by: Claude Opus 4.6 <noreply@anthropic.com>
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/mosquitto.c
@@ -59,6 +59,7 @@ static void cb_msg(struct mosquitto *mosq, void *userdata, const struct mosquitt
     (void) mosq;
     (void) userdata;
     if (msg->payloadlen) {
+        char *payload = strndup(msg->payload, msg->payloadlen);
         struct topic parsed_topic = parse_topic(msg->topic);
         if (strcmp(parsed_topic.action, "add") == 0) {
             ipset_add(parsed_topic.name, msg->payload);
@@ -67,6 +68,7 @@ static void cb_msg(struct mosquitto *mosq, void *userdata, const struct mosquitt
         }
         free(parsed_topic.action);
         free(parsed_topic.name);
+        free(payload);
     }
 }
 