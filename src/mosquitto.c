stintel/vallumd

commit 07f40fd0e5755294d6d6c1632313cae4aef5941c

mosquitto: use strdup instead of malloc and strcpy
Silences the following clang-tidy warnings:

/home/stijn/projects/vallumd/src/mosquitto.c:25:9: warning: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119 [clang-analyzer-security.insecureAPI.strcpy]
   25 |         strcpy(parsed_topic.name, strsep(&topic, "/"));
      |         ^~~~~~
/home/stijn/projects/vallumd/src/mosquitto.c:25:9: note: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119
   25 |         strcpy(parsed_topic.name, strsep(&topic, "/"));
      |         ^~~~~~
/home/stijn/projects/vallumd/src/mosquitto.c:26:9: warning: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119 [clang-analyzer-security.insecureAPI.strcpy]
   26 |         strcpy(parsed_topic.action, topic);
      |         ^~~~~~
/home/stijn/projects/vallumd/src/mosquitto.c:26:9: note: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119
   26 |         strcpy(parsed_topic.action, topic);
      |         ^~~~~~
/home/stijn/projects/vallumd/src/mosquitto.c:28:9: warning: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119 [clang-analyzer-security.insecureAPI.strcpy]
   28 |         strcpy(parsed_topic.name, topic);
      |         ^~~~~~

src/mosquitto.c
@@ -18,15 +18,12 @@ static struct topic parse_topic(char *topic)
 {
     struct topic parsed_topic;
 
-    parsed_topic.action = malloc(4);
-    parsed_topic.name = malloc(strlen(topic) + 1);
-
     if (strchr(topic, '/') != NULL) {
-        strcpy(parsed_topic.name, strsep(&topic, "/"));
-        strcpy(parsed_topic.action, topic);
+        parsed_topic.action = strdup(strsep(&topic, "/"));
+        parsed_topic.name = strdup(topic);
     } else {
-        strcpy(parsed_topic.name, topic);
-        strcpy(parsed_topic.action, "add");
+        parsed_topic.action = strdup(topic);
+        parsed_topic.name = strdup("add");
     }
 
     return parsed_topic;

commit 6b8fdf3e08091fd9912e84bb652f9f191e596ee7

mosquitto: fix parse_topic
While introducing strdup, action and topic name got accidentally
swapped. Fix that.

Fixes: 07f40fd0e575 ("mosquitto: use strdup instead of malloc and strcpy")
Reported-by: Claude Opus 4.6 <noreply@anthropic.com>
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

src/mosquitto.c
@@ -20,11 +20,11 @@ static struct topic parse_topic(char *topic)
     struct topic parsed_topic;
 
     if (strchr(topic, '/') != NULL) {
-        parsed_topic.action = strdup(strsep(&topic, "/"));
-        parsed_topic.name = strdup(topic);
-    } else {
+        parsed_topic.name = strdup(strsep(&topic, "/"));
         parsed_topic.action = strdup(topic);
-        parsed_topic.name = strdup("add");
+    } else {
+        parsed_topic.name = strdup(topic);
+        parsed_topic.action = strdup("add");
     }
 
     return parsed_topic;